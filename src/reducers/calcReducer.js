845b985f1eefb389207ad33875af0e1b500dd208
DESKTOP-LKA6RLL\Ivan <reinaldoivan777@gmail.com>
2018-12-18 00:45:24 +0700

    add localstorage to store calculation history

--- src/reducers/calcReducer.js
+++ src/reducers/calcReducer.js
@@ -1,4 +1,7 @@
-const initialState = {value:0, result: 0}
+const initialState = {
+    value:(localStorage.getItem('value') === null ? 0 : localStorage.getItem('value')), 
+    result: (localStorage.getItem('result') === null ? 0 : localStorage.getItem('result'))
+}
 
 const calcReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -7,12 +10,17 @@ const calcReducer = (state = initialState, action) => {
                 ...state, value: state.value == 0 ? action.text : state.value + action.text
             }
         case 'CLEAR': 
+            localStorage.setItem('result', 0)
+            localStorage.setItem('value', 0)
             return {
                 ...state, value: 0, result: 0
             }
         case 'EQUAL': 
+            let hasil = eval(action.value)
+            localStorage.setItem('result', hasil)
+            localStorage.setItem('value', action.value)
             return {
-                ...state, result: eval(action.value)
+                ...state, result: hasil
             }
         default: 
             return state
